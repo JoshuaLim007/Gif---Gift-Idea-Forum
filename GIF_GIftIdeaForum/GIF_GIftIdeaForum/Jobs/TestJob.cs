07db9744ec7d495173d013f61f5cc082c16b38b1
Joshua Lim <66184501+JoshuaLim007@users.noreply.github.com>
2020-10-14 20:19:28 -0700

    Framework Execution Order Update

--- GIF_GIftIdeaForum/GIF_GIftIdeaForum/Jobs/TestJob.cs
+++ GIF_GIftIdeaForum/GIF_GIftIdeaForum/Jobs/TestJob.cs
@@ -6,13 +6,35 @@ using System.Threading.Tasks;
 
 namespace GIF_GIftIdeaForum.Jobs
 {
+    [ExecutionOrder(0)] //Requires Execution Order Number
     [BindToClass(typeof(IndexModel))] //This ensures that this class is only ran when a specific page is opened
     //without this, the class would run anytime any pages open
     public class TestJob : Base
     {
         public override void Run()
         {
-            DebugLog("Invoked"); //this writes to VS console
+            DebugLog("Invoked 0"); //this writes to VS console
         }
     }
+
+    [ExecutionOrder(1)]
+    [BindToClass(typeof(IndexModel))]
+    public class TestJobSecond : Base
+    {
+        public override void Run()
+        {
+            DebugLog("Invoked 1"); 
+        }
+    }
+
+    [ExecutionOrder(2)]
+    [BindToClass(typeof(IndexModel))]
+    public class TestJobThird : Base
+    {
+        public override void Run()
+        {
+            DebugLog("Invoked 2");
+        }
+    }
+
 }
