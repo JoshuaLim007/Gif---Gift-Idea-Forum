8bd419091367ab68b1ee38761fc7bdb96510c237
Joshua Lim <66184501+JoshuaLim007@users.noreply.github.com>
2020-10-14 17:32:11 -0700

    Built skeleton of project
    YEE BOI

---
+++ GIF_GIftIdeaForum/GIF_GIftIdeaForum/Jobs/TestJob.cs
@@ -0,0 +1,18 @@
+﻿using GIF_GIftIdeaForum.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GIF_GIftIdeaForum.Jobs
+{
+    [BindToClass(typeof(IndexModel))] //This ensures that this class is only ran when a specific page is opened
+    //without this, the class would run anytime any pages open
+    public class TestJob : Base
+    {
+        public override void Run()
+        {
+            DebugLog("Invoked"); //this writes to VS console
+        }
+    }
+}
--- GIF_GIftIdeaForum/GIF_GIftIdeaForum/Pages/Index.cshtml.cs
+++ GIF_GIftIdeaForum/GIF_GIftIdeaForum/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -10,8 +11,8 @@ namespace GIF_GIftIdeaForum.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
 
+        private readonly ILogger<IndexModel> _logger;
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -19,7 +20,7 @@ namespace GIF_GIftIdeaForum.Pages
 
         public void OnGet()
         {
-
+            MainC.InitializeMainMethod(typeof(IndexModel));
         }
     }
 }
